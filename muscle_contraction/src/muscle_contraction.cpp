c320c18940accdbf1beb22f8a568fe0e2537b46e
Silas Natterer <st168958@stud.uni-stuttgart.de>
2023-02-20 01:40:37 +0100

    Added dynamic hyperelasticity case

---
+++ muscle_contraction/src/muscle_contraction.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "opendihu.h"
+
+int main(int argc, char *argv[])
+{
+  // 3D Laplace equation 0 = du^2/dx^2 + du^2/dy^2
+  
+  // initialize everything, handle arguments and parse settings from input file
+  DihuContext settings(argc, argv);
+  
+  MuscleContractionSolver<> equationDiscretized(settings);
+  
+  equationDiscretized.run();
+  
+  return EXIT_SUCCESS;
+}
